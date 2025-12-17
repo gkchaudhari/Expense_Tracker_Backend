4ba1855c011dd10e949e99341185021cb4071dc6
ganeshbyte <gc930954@gmail.com>
2025-12-17 08:38:25 +0530

    initial project setup

---
+++ Expense_Tracker/Expense_Tracker/Controllers/AuthController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Expense_Tracker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+    }
+}
